5043018cab82744ef06e640d6dcd9ed5c473c85a
Christoph Göttschkes <just.mychris@googlemail.com>
2013-04-11 20:49:37 +0200

    Changed the interface, url/id is now a positional argument

--- xbmc_command/youtube.py
+++ xbmc_command/youtube.py
@@ -11,14 +11,14 @@ class Command(core.Command):
 
   def call(self, args):
     xbmc_file = 'plugin://plugin.video.youtube/?action=play_video&videoid=%s'
-    if args.id:
-      xbmc_file = xbmc_file % args.id
-    else:
-      url = urlparse.urlparse(args.url)
+    try:
+      url = urlparse.urlparse(args.link)
       url_query = urlparse.parse_qs(url.query)
       if not 'v' in url_query:
         raise core.CommandException("No youtube video id found in given url.")
       xbmc_file = xbmc_file % url_query['v'][0]
+    except:
+      xbmc_file = xbmc_file % args.link
 
     if args.quality:
       xbmc_file += '&quality=%s' % args.quality
@@ -29,18 +29,13 @@ class Command(core.Command):
     parser.prog = '%s youtube' % core.prog
     parser.description = 'Open a youtube video file.'
 
-    group= parser.add_mutually_exclusive_group(required=True)
-
-    group.add_argument('--id', dest='id', metavar='<youtube id>',
-        help='the youtube video id')
-
-    group.add_argument('--url', dest='url', metavar='<url>',
-        help='the youtube video url')
+    parser.add_argument('link', metavar='<link>',
+        help='the url or the youtube video id')
 
     parser.add_argument('--quality', dest='quality', default=None,
         choices=['1080p', '720p', 'low'],
         help='the quality to use. By default, no quality information will be \
-            send and the plugin will choose one.')
+            send and the XBMC plugin will choose one.')
 
   @property
   def short_description(self):
