ec39d5ae5f98c301953614215729a0b9a0493c1c
Christoph Göttschkes <just.mychris@googlemail.com>
2013-04-11 21:01:56 +0200

    now works with positional arguments (defaults to toggle)

--- xbmc_command/mute.py
+++ xbmc_command/mute.py
@@ -5,14 +5,22 @@ from . import core
 class Command(core.Command):
 
   def call(self, args):
-    self.xbmc.Application.SetMute({'mute': 'toggle'})
+    if args.state == 'yes' or args.state == 'y':
+      args.state = True
+    elif args.state == 'no' or args.state == 'n':
+      args.state = False
+    self.xbmc.Application.SetMute({'mute': args.state}) 
 
   def create_parser(self, parser):
     parser.prog = '%s mute' % core.prog
-    parser.description = 'Toogle mute/unmute.'
+    parser.description = 'Set the mute state to mute/unmute or toggle it..'
+
+    parser.add_argument('state', metavar='<state>', nargs='?',
+        choices=['yes', 'y', 'no', 'n', 'toggle', 't'], default='toggle',
+        help='the mute state. Either \'yes\', \'no\' or \'toggle\', default toggle')
 
   @property
   def short_description(self):
-    return 'Toggle mute'
+    return 'Set the mute state'
 
 # vim: ft=python ts=2 sts=2 sw=2 et:
